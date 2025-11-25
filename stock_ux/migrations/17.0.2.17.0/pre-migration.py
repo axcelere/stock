86817b4aaea870674d20ada75b923072c8265b2f
Rafael Aguero Baquero <raguero@quanam.com>
2025-11-25 18:18:54 -0300

    [FIX] l10n_ar TRASH

--- stock_ux/migrations/17.0.2.17.0/pre-migration.py
+++ stock_ux/migrations/17.0.2.17.0/pre-migration.py
@@ -7,6 +7,7 @@ def migrate(env, version):
     The objective of this is delete the original view form the module how bring the functionality
     adding in the previous commit
     """
+    self.env.ref('stock_ux.view_move_line_form').unlink()
     l10n_latam_use_checkbooks = env['ir.ui.view'].search([('arch_db', 'like', 'payment_status'), ('name', 'ilike', 'stock.move.line')])
     env['ir.ui.view'].search([('inherit_id', 'in', l10n_latam_use_checkbooks.ids)]).unlink()
     l10n_latam_use_checkbooks.unlink()
