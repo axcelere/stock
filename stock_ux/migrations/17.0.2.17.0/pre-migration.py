892a5c2fe09be8d5c51fba6d273d3aa06362df09
Rafael Aguero Baquero <raguero@quanam.com>
2025-11-25 17:38:58 -0300

    [FIX] l10n_ar TRASH

---
+++ stock_ux/migrations/17.0.2.17.0/pre-migration.py
@@ -0,0 +1,12 @@
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    """
+    The objective of this is delete the original view form the module how bring the functionality
+    adding in the previous commit
+    """
+    l10n_latam_use_checkbooks = env['ir.ui.view'].search([('arch_db', 'like', 'payment_status'), ('name', 'ilike', 'stock.move.line')])
+    env['ir.ui.view'].search([('inherit_id', 'in', l10n_latam_use_checkbooks.ids)]).unlink()
+    l10n_latam_use_checkbooks.unlink()
